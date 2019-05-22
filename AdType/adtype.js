fc90bd7cf636fdfb1bbc9a203e1c6dfdd63daf4c
WebModerna@github.com <edgardogalletto@gmail.com>
2019-05-22 14:35:46 -0300

    Creando unos ejercicios para AdType

---
+++ AdType/adtype.js
@@ -0,0 +1,39 @@
+// Making the AdType's test.
+class multiplesFunciones
+{
+	constructor( texto, numero )
+	{
+		this.texto = texto;
+		this.numero = numero;
+	}
+
+	// REVERSE A STRING
+	reversar()
+	{
+		
+	}
+
+	// VALIDATE A PALINDROME
+	palindrome()
+	{
+
+	}
+
+	// REVERSE AN INTEGER
+	reverseInterger()
+	{
+
+	}
+
+	// CAPITALIZE LETTERS
+	capitalizar()
+	{
+
+	}
+
+	// MAX CHARACTER
+	maximizarCaracter()
+	{
+
+	}
+}
